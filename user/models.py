a10a5f88e1d1018881e7dfd84f799469c0176231
admin <toshpulatovm73@gmail.com>
2025-01-02 11:45:48 +0500

    Initial commit

--- user/models.py
+++ user/models.py
@@ -1,10 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db.models import CharField, IntegerField, TextChoices, TextField, ImageField, Model, DateTimeField, \
-    ForeignKey, CASCADE
+    ForeignKey, SET_NULL
 
-class Base(Model):
-    created_at=DateTimeField(auto_now_add=True)
-    updated_at=DateTimeField(auto_now=True)
 class User(AbstractUser):
     class TYPE(TextChoices):
         ADMIN='admin','Admin',
@@ -15,5 +12,5 @@ class User(AbstractUser):
     role=CharField(max_length=50,choices=TYPE.choices,default=TYPE.USER)
     about=TextField(null=True,blank=True)
     image=ImageField(upload_to='avatar/%y/%m/%d',null=True,blank=True)
-    base=ForeignKey('user.Base',on_delete=CASCADE,null=True,blank=True)
+    film_history=ForeignKey('film.Film',related_name='film_history',on_delete=SET_NULL,null=True,blank=True)
 
